f0517e2d0d804bb629f85c8206fc2db4b702c23d
tormobr <tormod.brandshoi@gmail.com>
2019-06-04 19:03:24 +0200

    p12 done.

---
+++ p12.py
@@ -0,0 +1,44 @@
+
+# What is the value of the first triangle number to have over five hundred divisors?
+
+
+def sieve(n):
+    primes = []
+    bitmap = [1 for i in range(n+1)]
+    x = 2
+    while x *x <= n:
+        if bitmap[x]:
+            for i in range(x*2, n+1, x):
+                bitmap[i] = 0
+        x += 1
+
+    for i in range(2, n):
+        if bitmap[i]:
+            primes.append(i)
+    return primes
+
+def facotrize(n, primes):
+    tmp = n
+    i = 0
+    res = 1
+    while primes[i]**2 < n:
+        expo = 1
+        while tmp % primes[i] == 0:
+            expo += 1
+            tmp /= primes[i]
+        i += 1
+        res *= expo
+    return res
+
+
+
+primes = sieve(200000)
+
+i = 0
+check = 0
+while facotrize(check, primes) < 500:
+    i += 1 
+    check += i
+print("result number: ", check)
+print("number of factors: ", facotrize(check, primes))
+
