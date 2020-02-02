b4573bd0384e0399d774c6431d976bd4b91c5838
tormobr <tormod.brandshoi@gmail.com>
2020-02-02 22:23:21 +0100

    made it niz

--- 053/p053.py
+++ 053/p053.py
@@ -1,21 +1,14 @@
 import math
-from itertools import combinations
 
 def solve():
     res = 0
-    res = [nCr(i,j) for j in range(1,i+1) for i in range(1,101)]
+    res = [nCr(i,j) for i in range(1,101) for j in range(1,i+1)]
     return len(list(filter(lambda x: x > 1000000, res)))
-    for i in range(1, 101):
-        for j in range(1, i+1):
-            if nCr(i,j) > 1000000:
-                res += 1
-    return res
-            
+
 def nCr(n, r):
     top = (math.factorial(n))
     bottom = math.factorial(r) * math.factorial(n - r)
     return top / bottom
 
 
-print(nCr(5,3))
 print(solve())
