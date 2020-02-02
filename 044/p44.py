c4f2869cd04537cae033f15cc59a24823d21131f
tormobr <tormod.brandshoi@gmail.com>
2020-02-02 14:51:20 +0100

    some small changes

--- 044/p44.py
+++ 044/p44.py
@@ -8,15 +8,13 @@ def gen_pents(num):
 def solve(n):
     pents = gen_pents(n+1)
     pent_set = set(pents)
-    res = 1000000000
-    for i in range(n):
+    i = 0
+    while True:
         for j in range(i):
             D = pents[i] - pents[j]
             P = pents[i] + pents[j]
-            #print(pents[i], pents[j], D, P)
-            #time.sleep(.4)
             if D in pent_set and P in pent_set:
                 return D
+        i += 1
     return res
-
 print(solve(3000))
