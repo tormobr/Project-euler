39d96ce96edce9280c499a76f5bbae03261c04a0
tormobr <tormod.brandshoi@gmail.com>
2020-02-03 15:08:20 +0100

    enhancements

--- 057/p057.py
+++ 057/p057.py
@@ -2,16 +2,12 @@
 
 def solve():
     fracs = gen_frac(1000)
-    res = 0
-    for (n, d) in fracs:
-        if len(str(n)) > len(str(d)):
-            res += 1
-    return res
+    return len([(n,d) for (n,d) in fracs if len(str(n)) > len(str(d))])
      
 
 def gen_frac(x):
     ret =[(1, 1)]
-    for i in range(x):
+    for i in range(x-1):
         n, d = ret[i]
         ret.append((n+(2*d), n+d))
     return ret 
