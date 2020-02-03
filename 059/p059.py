b5b30d06c1d2aba33389a4c70476487aaee52f78
tormobr <tormod.brandshoi@gmail.com>
2020-02-03 17:29:41 +0100

    some nice enhancements

--- 059/p059.py
+++ 059/p059.py
@@ -1,27 +1,19 @@
+import sys
 import time
 from itertools import permutations
 from itertools import cycle
 
 def solve(data):
-    combs = permutations([i for i in range(97, 123)], 3)  
+    combs = permutations([i for i in range(ord("a"), ord("z"))], 3)  
     for c in combs:
-        res = []
-        cyc = cycle(c)
-        for x in data:
-            k = next(cyc)
-            res.append(x ^ k)
-            #print(x, k, res[-1])
-        if check(res):
-            return sum(res)
-        #time.sleep(.1)
+        current = [data[i] ^ c[i%3] for i in range(len(data))]
+        if check(current):
+            return sum(current)
 
 def check(a):
-    count = 0
-    ret = ""
-    for s in a:
-        ret += chr(s)
-    print(ret)
-    return ret.count(" ") > 200
+    filt = list(filter(lambda s: s in range(64,123) or s == 32, a))
+    return len(filt) > len(a) * 0.9
+            
 
     
 def read_file(filename):
