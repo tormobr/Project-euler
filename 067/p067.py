c09a577fd31b6f16b546f7b5063777ab92803481
tormobr <tormod.brandshoi@gmail.com>
2020-02-04 18:04:41 +0100

    started on 67

---
+++ 067/p067.py
@@ -0,0 +1,20 @@
+
+import time
+
+def solve():
+    data = read_file()
+    return rec(data, 0, 0)
+
+def rec(data, level, i):
+    print(level, i)
+    if level == len(data)-1:
+        return data[level][i]
+
+    left = rec(data, level+1, i)
+    right = rec(data, level+1, i+1)
+    return data[level][i] + max(left, right)
+
+def read_file():
+    return [[int(x) for x in line.split()] for line in open("input.txt")]
+
+print(solve())
