740c0123d3d4cbe2e46fe0ab451b757ea0e2a1b3
tormobr <tormod.brandshoi@gmail.com>
2020-02-04 14:07:01 +0100

    some cleaning

--- 061/p061.py
+++ 061/p061.py
@@ -1,53 +1,45 @@
+import sys
 from itertools import permutations
 from collections import defaultdict
 import time
 
 
 def solve():
-    nums = gen(1000, 9999)
+    nums = gen()
     d = create_dict(nums)
     for k in d.keys():
         rec(d.copy(), k, [])
 
-
 def rec(d, k, path):
-    print(k)
-    childs = d[k]
     path.append(k)
     if len(path) == 6:
-        print("possibly:", path)
-        if match(k[1], path[0][1]):
-            if len(set([t for t,v in path])) == 6:
-                print(str(k)[2:], str(path[0])[:2])
-                print("found the shit!!!!", path, sum([v for t,v in path]))
-                time.sleep(10)
-    elif len(path) > 6:
-        return
-    for c in childs:
-        rec(d, c, path.copy())
-    
+        if path[0] in d[k] and len(set([t for t,v in path])) == 6:
+            print("result: ",path, sum([v for t,v in path]))
+            sys.exit(0)
+    else:
+        for c in d[k]:
+            rec(d, c, path.copy())
 
 def match(a, b):
     return a % 100 == b // 100
 
-def gen(s, e):
-    i = 1
+
+def gen():
     ret = []
-    for i in range(19,141):
-        ret += [(3, int(i*(i+1)/2)),
-        (4, int(i**2)),
-        (5, int(i*((3*i)-1)/2)),
-        (6, int(i*((2*i)-1))),
-        (7, int(i*((5*i)-3)/2)),
-        (8, int(i*((3*i)-2)))]
-        i += 1
-    return list(filter(lambda x: len(str(x[1])) == 4, ret))
+    for i in range(19,144):
+        ret += [(3, i*(i+1)/2),
+            (4, i**2),
+            (5, i*((3*i)-1)/2),
+            (6, i*((2*i)-1)),
+            (7, i*((5*i)-3)/2),
+            (8, i*((3*i)-2))]
+    return list(map(lambda x: (x[0], int(x[1])), ret))
 
 def create_dict(a):
     d = defaultdict(lambda: [])
     for t, v in a:
         for t1, v1 in a:
-            if str(v)[2:] == str(v1)[:2] and t != t1:
+            if match(v, v1) and t != t1:
                 d[(t,v)].append((t1, v1))
     return d
 
