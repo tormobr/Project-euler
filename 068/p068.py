aaf646f628988a23a719177e4fee813da440b856
tormobr <tormod.brandshoi@gmail.com>
2020-02-05 18:53:16 +0100

    kinda hacky but workspython p068.py

--- 068/p068.py
+++ 068/p068.py
@@ -3,19 +3,13 @@ from itertools import permutations
 
 def solve():
     res = 0
-    nums = [n for n in range(1,11)]
-    perms = permutations(nums, 5)
+    perms = permutations([1,2,3,4,5])
     for p in perms:
-        x = [k for k in range(1,11) if k not in p]
-        perms2 = permutations(x)         
+        perms2 = permutations([6,7,8,9, 10])
         for p2 in perms2:
             hax = check(p, p2)
             if hax[0]:
-                
-                if len(hax[2]) == 16:
-                    print(p, p2, hax[2][::-1])
-                    res = int(hax[2][::-1])
-                    time.sleep(1)
+                res = int(hax[2])
 
     return res
 
@@ -24,8 +18,9 @@ def check(p, p2):
     digit = ""
     for i in range(len(p)):
         res.append(p[i]+p[(i+1)%5]+p2[i])
-        digit += str(p[i]) + str(p[(i+1)%5]) + str(p2[i])
+        digit += str(p2[i])+ str(p[(i+1)%5]) + str(p[i])
     return len(set(res)) == 1, res, digit
 
-
+s = time.time()
 print(solve())
+print("runtime: ", time.time()-s)
