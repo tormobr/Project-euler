98a2e9f795ef570ca2d69b92e87c8cb764cf4ce0
tormobr <tormod.brandshoi@gmail.com>
2020-02-10 20:37:47 +0100

    some small enhancements

--- 075/p075.py
+++ 075/p075.py
@@ -1,31 +1,21 @@
 from collections import defaultdict
 import math
+from itertools import product
 
 def solve(n):
-    d = defaultdict(int)
-    res = 0 
-    trips = gen_trips(n)
-    for a,b,c in trips:
-        tmp = a+b+c
-        tmp0 = tmp
-        while tmp < n:
-            d[tmp] += 1
-            tmp += tmp0
-    for k, v in d.items():
-        if v == 1:
-            res += 1
-    return res
-
+    d = gen_trips(n)
+    return sum(1 for v in d.values() if v == 1)
 
 def gen_trips(lim):
-    res = set()
-    for m in range(2, int(math.sqrt(lim))):
+    d = defaultdict(int)
+    for m in range(int(math.sqrt(lim))):
         for n in range(1, m):
-            if (n + m) % 2 == 1 and math.gcd(n,m) == 1:
-                a = m**2 - n**2
-                b = 2*m*n
-                c = m**2 + n**2
-                res.add((a,b,c))
-    return res
+            if (n + m) % 2 != 1 or math.gcd(n,m) != 1:
+                continue
+            x = tmp = (m**2 - n**2) + (2*m*n) +(m**2 + n**2)
+            while x < lim:
+                d[x] += 1
+                x += tmp
+    return d
 
 print(solve(1500000))
